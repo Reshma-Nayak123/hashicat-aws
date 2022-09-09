4cfc99fffc52ea4368760712d999ecc09d4650f2
Reshma-Nayak123 <106798480+Reshma-Nayak123@users.noreply.github.com>
2022-09-09 16:17:59 +0530

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,4 +1,4 @@
-module "s3-bucket_terraform-google-first" {
-  source  = "app.terraform.io/INSTRUQT1/s3-bucket/aws//modules/terraform-google-first"
-  version = "1.0.4"
+module "s3-bucket_notification" {
+  source  = "app.terraform.io/INSTRUQT1/s3-bucket/aws//modules/notification"
+  version = "1.0.0"
 }
